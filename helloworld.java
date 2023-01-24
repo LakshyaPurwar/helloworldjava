9346ba6603cd76d72796c7e9065a6d9739548c4d
Lakshya Purwar <78524327+LakshyaPurwar@users.noreply.github.com>
2023-01-24 22:36:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World!!Abhi kahan kuch kiya hai");
+    System.out.println("Hello World!!gb update");
     }
 }
